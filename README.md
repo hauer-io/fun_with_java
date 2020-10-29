f48eeb83fcc415cad2d2c92407a6147aad721c67
Jan Hauer <Jan.Hauer@EXXETA.com>
2020-10-29 23:24:31 +0100

    Workshop (After)

--- README.md
+++ README.md
@@ -15,3 +15,4 @@
 
 ## Usage
 * [JCON (2020) Session](https://github.com/hauer-io/jfun/tree/2020-JCON-SESSION)
+* [JCON (2020) Workshop](https://github.com/hauer-io/jfun/tree/2020-JCON-WORKSHOP)
--- workshop/task5/core5/src/main/java/io/hauer/demo/jfun/RecipeTransformer.java
+++ workshop/task5/core5/src/main/java/io/hauer/demo/jfun/RecipeTransformer.java
@@ -1,29 +1,44 @@
 package io.hauer.demo.jfun;
 
+import cyclops.arrow.Kleisli;
 import cyclops.control.Option;
 import cyclops.reactive.ReactiveSeq;
+import org.springframework.http.ResponseEntity;
 
 import java.util.function.Function;
 
+import static com.oath.cyclops.hkt.DataWitness.option;
+import static cyclops.instances.control.OptionInstances.monad;
+
 public class RecipeTransformer {
+    private static final RecipeService service = new RecipeService(null);
 
-    public static Function<RecipeService, ReactiveSeq<Ingredient>> loadIngredients(final long id) {
-        return service -> service
+    public static Kleisli<option, RecipeService, ReactiveSeq<Ingredient>> loadIngredients(final long id) {
+        return Kleisli.of(monad(), service -> service
                 .get(id)
-                .map(Recipe::getIngredients)
-                .orElse(ReactiveSeq.empty());
+                .map(Recipe::getIngredients));
     }
 
-    public static Function<RecipeService, Option<Recipe>> updateIngredients(final long id,
+    public static Kleisli<option, RecipeService, Recipe> updateIngredients(final long id,
             final ReactiveSeq<Ingredient> ingredients) {
-        return service -> service
+        return Kleisli.of(monad(), service -> service
                 .get(id)
-                .map(recipe -> recipe.withIngredients(ingredients));
+                .map(recipe -> recipe.withIngredients(ingredients)));
     }
 
-    public static Function<RecipeService, Function<RecipeService, Option<Recipe>>> transformer(final long id,
+    public static Kleisli<option, RecipeService, Recipe> transformer(final long id,
             final Function<Ingredient, Ingredient> t) {
-        return loadIngredients(id).andThen(ingredients -> updateIngredients(id, ingredients.map(t)));
+        return loadIngredients(id).flatMapK(ingredients -> updateIngredients(id, ingredients.map(t)));
+    }
+
+    private static void test() {
+        final var x = transformer(10, Function.identity()).apply(service);
+        final var r = Option
+                .narrowK(x)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity
+                        .notFound()
+                        .build());
     }
 }
 
